692842c1df7b9882ff43ae3dd4c9cf7a89f81901
Romain Létendart <romain.letendart@polyconseil.fr>
2020-10-01 17:09:55 +0200

    release version 0.1.5+poly.2020.1.gitd0c1877

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='A teeny Python library for creating Python dicts from '
                 'protocol buffers and the reverse. Useful as an intermediate step '
                 'before serialisation (e.g. to JSON).',
-    version='0.1.5+poly.2020.1.git7d31546',
+    version='0.1.5+poly.2020.1.gitd0c1877',
     author='Ben Hodgson',
     author_email='ben@benhodgson.com',
     url='https://github.com/benhodgson/protobuf-to-dict',
